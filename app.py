532a61cc04c5aef7ef16de50bed3b40728fc487b
Ashutosh Chandra <64329364+chandra56ashu@users.noreply.github.com>
2026-01-23 14:59:18 +0000

    Update app.py

--- app.py
+++ app.py
@@ -152,4 +152,6 @@ def index():
     return render_template("index.html", result=result, error=error)
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
+
+
